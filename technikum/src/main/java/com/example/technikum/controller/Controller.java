5e44f582bb67f1a27062e7665b3409a7bd56dd92
Philip <se22m039@technikum-wien.at>
2022-11-02 14:58:18 +0100

    Add Logback config and tests

--- technikum/src/main/java/com/example/technikum/controller/Controller.java
+++ technikum/src/main/java/com/example/technikum/controller/Controller.java
@@ -2,6 +2,8 @@ package com.example.technikum.controller;
 
 import com.example.technikum.service.Services;
 import com.github.javafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,14 @@ public class Controller {
 
     @Autowired
     private Services services;
+    private static final Logger logger
+            = LoggerFactory.getLogger(Controller.class);
 
     @GetMapping("/helloPokemon")
     public String fakcer() {
         Faker faker = new Faker();
         String name = faker.pokemon().name();
+        logger.info("Name des Pokemon:" + name);
         return "Hallo," + name;
     }
 
